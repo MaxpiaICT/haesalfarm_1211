ba677f3e409a70500d68dcf78369886292f74edd
jisun <maxpiaictntec@gmail.com>
2025-12-12 12:08:53 +0900

    revert

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  // ⚠️ GitHub 저장소 이름과 정확히 일치시켜야 합니다.
+  base: '/haesalfarm_1211/',
   plugins: [react()],
 })
